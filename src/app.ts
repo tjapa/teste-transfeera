b40fed527efef3510cb12c621bc9c9a402f6cc67
tjapa <nicotamalu@gmail.com>
2024-05-07 20:44:37 -0300

    feat: add express and run server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('Hello World!')
+import express from 'express'
+
+const app = express()
+
+const PORT = 3000
+
+app.listen(3000, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+})
