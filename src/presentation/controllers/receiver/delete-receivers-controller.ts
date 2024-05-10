1ba4875f7b83bf0ea71edf53f2f94efb07b9cf98
tjapa <nicotamalu@gmail.com>
2024-05-10 13:54:07 -0300

    feat: add DeleteReceiversController

---
+++ src/presentation/controllers/receiver/delete-receivers-controller.ts
@@ -0,0 +1,45 @@
+import { DeleteReceiversRequest } from '@/presentation/models/delete-receivers-request'
+import {
+  forbidden,
+  ok,
+  serverError,
+} from '@/presentation/helpers/http/http-helper'
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  Validator,
+} from '@/presentation/protocols'
+import { DeleteReceiversUseCase } from '@/use-cases/protocols/delete-receivers-use-case'
+
+export class DeleteReceiversController implements Controller {
+  constructor(
+    private readonly deleteReceivers: DeleteReceiversUseCase,
+    private readonly validator: Validator<DeleteReceiversRequest>,
+  ) { }
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    let deleteReceiversRequest: DeleteReceiversRequest
+
+    try {
+      deleteReceiversRequest = this.validator.validate(httpRequest.body)
+    } catch (error) {
+      return forbidden(error as Error)
+    }
+
+    try {
+      const deletedReceivers = await this.deleteReceivers.deleteReceivers(
+        deleteReceiversRequest,
+      )
+
+      const deletedReceiversFormatted = {
+        deleted_receiver_ids: deletedReceivers.deletedReceiverIds,
+        not_found_receiver_ids: deletedReceivers.deletedReceiverIds,
+      }
+
+      return ok(deletedReceiversFormatted)
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
