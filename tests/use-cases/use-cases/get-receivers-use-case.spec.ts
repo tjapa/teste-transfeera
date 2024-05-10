bd3176e12763f09b0c5d73756ff7c09829442a39
tjapa <nicotamalu@gmail.com>
2024-05-10 12:05:44 -0300

    test: add check return value test for getReceiversUseCase

--- tests/use-cases/use-cases/get-receivers-use-case.spec.ts
+++ tests/use-cases/use-cases/get-receivers-use-case.spec.ts
@@ -4,6 +4,7 @@ import {
 } from '@/use-cases/protocols/get-receivers-use-case'
 import { mockGetReceiversRepository } from '@/tests/repository/mocks/mock-get-receivers-repository'
 import { GetReceivers } from '@/use-cases/use-cases/get-receivers-use-case'
+import { mockReceiverRascunhoCPF } from '../mocks/mock-receiver'
 
 interface SutType {
   sut: GetReceiversUseCase
@@ -22,6 +23,28 @@ const makeSut = (): SutType => {
 
 describe('Get Receivers Use Case', () => {
   describe('getReceivers()', () => {
+    test('Should return the receivers found by repository', async () => {
+      const { sut, getReceiversRepositoryStub } = makeSut()
+      const correctParam: FiltersGetReceivers = {
+        status: 'RASCUNHO',
+        pixKey: 'any_pix_key',
+        pixKeyType: 'CPF',
+        name: 'any_name',
+      }
+      const mockReturnValueGetReceiversRepository = [
+        mockReceiverRascunhoCPF(),
+        mockReceiverRascunhoCPF(),
+        mockReceiverRascunhoCPF(),
+      ]
+      jest
+        .spyOn(getReceiversRepositoryStub, 'getReceivers')
+        .mockReturnValueOnce(
+          Promise.resolve(mockReturnValueGetReceiversRepository),
+        )
+      const receiversGetted = await sut.getReceivers(correctParam)
+      expect(receiversGetted).toEqual(mockReturnValueGetReceiversRepository)
+    })
+
     test('Should call GetReceiversRepository with correct param', async () => {
       const { sut, getReceiversRepositoryStub } = makeSut()
       const correctParam: FiltersGetReceivers = {
